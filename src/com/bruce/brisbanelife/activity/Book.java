91b09e45e5ce99be4843b3791eda5f978afd1b38
ppbao <bruce.liu.02@gmail.com>
2014-12-17 08:12:15 +1000

    modify book, add student.

--- src/com/bruce/brisbanelife/activity/Book.java
+++ src/com/bruce/brisbanelife/activity/Book.java
@@ -2,4 +2,17 @@ package com.bruce.brisbanelife.activity;
 
 public class Book {
 	private String name;
+	private String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
---
+++ src/com/bruce/brisbanelife/activity/StudentActivity.java
@@ -0,0 +1,5 @@
+package com.bruce.brisbanelife.activity;
+
+public class StudentActivity {
+	private String tag="strudent activity";
+}
